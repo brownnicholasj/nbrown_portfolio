08664015d8a1b5f41803d1d69f8e07eb88290000
Nicholas Brown <brown.nicholasj@gmail.com>
2021-08-01 18:48:10 -0500

    updated about react file to have data in it

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export default function About() {
-	return <h1>about me section</h1>;
+	return (
+		<div>
+			<h2>about me section</h2>
+			<p>
+				Multidisciplinary Full Stack Web Developer and Product Owner that
+				combines experience and traditional learning methods to solve critical
+				problems for users.
+			</p>
+			<p>
+				Over 10 years of Transportation and Supply Chain experience, along with
+				a Full Stack Development Certificate from the University of Kansas.
+			</p>
+			<p>
+				Excited to advance products with both business and technical aspects to
+				optimize outcomes for all stakeholders.
+			</p>
+		</div>
+	);
 }
