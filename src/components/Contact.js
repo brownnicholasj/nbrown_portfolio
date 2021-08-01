c576df8b97cbff432777ea910c72664f72bb78dc
Nicholas Brown <brown.nicholasj@gmail.com>
2021-08-01 18:57:22 -0500

    updated info inside contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,48 @@
 import React from 'react';
 
 export default function Contact() {
-	return <h1>contact section</h1>;
+	return (
+		<div>
+			<h2>contact section</h2>
+			<figure id='resume'>
+				<a
+					href='https://brownnicholasj.github.io/nbrown_portfolio/assets/images/NicholasBrown_resume.pdf'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<img
+						src='https://img.icons8.com/ios/452/open-resume.png'
+						alt='resume logo'
+					/>
+					<p>Resume</p>
+				</a>
+			</figure>
+			<figure id='linkedin'>
+				<a
+					href='https://www.linkedin.com/in/nibrown/'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<img
+						src='https://cdn2.iconfinder.com/data/icons/social-media-with-original-colors/256/icon-linkedin.png'
+						alt='linkedin logo'
+					/>
+					<p>Linkedin</p>
+				</a>
+			</figure>
+			<figure id='github'>
+				<a
+					href='https://github.com/brownnicholasj'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<img
+						src='https://cdn1.iconfinder.com/data/icons/social-media-vol-2-1/24/_github_copy-256.png'
+						alt='github logo'
+					/>
+					<p>GitHub</p>
+				</a>
+			</figure>
+		</div>
+	);
 }
