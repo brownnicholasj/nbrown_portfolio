a712495d12ccc4155dc98d3a18bf26e5d7d0d070
Nicholas Brown <brown.nicholasj@gmail.com>
2021-08-01 19:38:56 -0500

    work section filled out and rendering data properly

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import jeopardyimage from '../images/jeopardy.jpg';
+import eventimage from '../images/eventplanning.jpg';
+import weatherimage from '../images/weatherdash.jpg';
+
+const projectArr = [
+	{
+		name: 'Jeopardy!',
+		link: 'https://brownnicholasj.github.io/jeopardy-game/',
+		image: jeopardyimage,
+		git: 'https://github.com/brownnicholasj/jeopardy-game',
+	},
+	{
+		name: 'The Event Planning',
+		link: 'https://the-event-planning.herokuapp.com/',
+		image: eventimage,
+		git: 'https://github.com/brownnicholasj/the-event-planning',
+	},
+	{
+		name: 'Weather Dashboard',
+		link: 'https://brownnicholasj.github.io/weather_dashboard/',
+		image: weatherimage,
+		git: 'https://github.com/brownnicholasj/weather_dashboard',
+	},
+];
 
 export default function Work() {
 	return (
-		<div>
+		<section>
 			<h2>work section</h2>
-		</div>
+			{projectArr.map((value, index) => {
+				return (
+					<div id='appsection'>
+						<h3 id={index}>{value.name}</h3>
+						<a href={value.link} target='_blank' rel='noreferrer'>
+							<img id={index} src={value.image} alt='screenshot of work' />
+						</a>
+						<a
+							href={value.git}
+							className='repo'
+							target='_blank'
+							rel='noreferrer'
+						>
+							<h4>Repository</h4>
+						</a>
+					</div>
+				);
+			})}
+		</section>
 	);
 }
