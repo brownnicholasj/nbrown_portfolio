1c3da22574dbc7c40d03fd1ee4471a9d12dff252
Nicholas Brown <brown.nicholasj@gmail.com>
2021-08-02 21:06:21 -0500

    finished adding basic CSS with contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,45 +5,47 @@ export default function Contact() {
 	return (
 		<section id='contact'>
 			<h2>Contact Me</h2>
-			<figure id='resume'>
-				<a
-					href='https://brownnicholasj.github.io/nbrown_portfolio/assets/images/NicholasBrown_resume.pdf'
-					target='_blank'
-					rel='noreferrer'
-				>
-					<img
-						src='https://img.icons8.com/ios/452/open-resume.png'
-						alt='resume logo'
-					/>
-					<p>Resume</p>
-				</a>
-			</figure>
-			<figure id='linkedin'>
-				<a
-					href='https://www.linkedin.com/in/nibrown/'
-					target='_blank'
-					rel='noreferrer'
-				>
-					<img
-						src='https://cdn2.iconfinder.com/data/icons/social-media-with-original-colors/256/icon-linkedin.png'
-						alt='linkedin logo'
-					/>
-					<p>Linkedin</p>
-				</a>
-			</figure>
-			<figure id='github'>
-				<a
-					href='https://github.com/brownnicholasj'
-					target='_blank'
-					rel='noreferrer'
-				>
-					<img
-						src='https://cdn1.iconfinder.com/data/icons/social-media-vol-2-1/24/_github_copy-256.png'
-						alt='github logo'
-					/>
-					<p>GitHub</p>
-				</a>
-			</figure>
+			<div>
+				<figure id='resume'>
+					<a
+						href='https://brownnicholasj.github.io/nbrown_portfolio/assets/images/NicholasBrown_resume.pdf'
+						target='_blank'
+						rel='noreferrer'
+					>
+						<img
+							src='https://img.icons8.com/ios/452/open-resume.png'
+							alt='resume logo'
+						/>
+						<p>Resume</p>
+					</a>
+				</figure>
+				<figure id='linkedin'>
+					<a
+						href='https://www.linkedin.com/in/nibrown/'
+						target='_blank'
+						rel='noreferrer'
+					>
+						<img
+							src='https://cdn2.iconfinder.com/data/icons/social-media-with-original-colors/256/icon-linkedin.png'
+							alt='linkedin logo'
+						/>
+						<p>Linkedin</p>
+					</a>
+				</figure>
+				<figure id='github'>
+					<a
+						href='https://github.com/brownnicholasj'
+						target='_blank'
+						rel='noreferrer'
+					>
+						<img
+							src='https://cdn1.iconfinder.com/data/icons/social-media-vol-2-1/24/_github_copy-256.png'
+							alt='github logo'
+						/>
+						<p>GitHub</p>
+					</a>
+				</figure>
+			</div>
 		</section>
 	);
 }
