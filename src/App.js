f3be7786f0b760f9c79b6cbc10684b67ff684f66
Nicholas Brown <brown.nicholasj@gmail.com>
2021-09-05 20:40:57 -0500

    update router

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
 			<Navbar />
 			<Switch>
 				<Route exact path='/' component={Home} />
+				<Route exact path='/nbrown_portfolio' component={Home} />
 				<Route exact path='/experience' component={Experience} />
 				<Route exact path='/portfolio' component={Portfolio} />
 				<Route exact path='/contact' component={Contact} />
