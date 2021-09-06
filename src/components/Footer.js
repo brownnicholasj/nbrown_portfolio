6f5b70e6c8a0725324b16a9820414917701bee5d
Nicholas Brown <brown.nicholasj@gmail.com>
2021-09-05 21:10:07 -0500

    updated footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import GitHub from '@material-ui/icons/GitHub';
 import LinkedIn from '@material-ui/icons/LinkedIn';
 import Resume from '@material-ui/icons/Description';
+import resume from '../images/NicholasBrown_resume.pdf';
 
 const useStyles = makeStyles({
 	bottomNavContainer: {
@@ -26,9 +28,21 @@ const Footer = () => {
 
 	return (
 		<BottomNavigation className={classes.bottomNavContainer}>
-			<BottomNavigationAction icon={<GitHub />} className={classes.root} />
-			<BottomNavigationAction icon={<LinkedIn />} className={classes.root} />
-			<BottomNavigationAction icon={<Resume />} className={classes.root} />
+			<BottomNavigationAction
+				icon={<GitHub />}
+				className={classes.root}
+				onClick={() => window.open('https://github.com/brownnicholasj')}
+			/>
+			<BottomNavigationAction
+				icon={<LinkedIn />}
+				className={classes.root}
+				onClick={() => window.open('https://www.linkedin.com/in/nibrown/')}
+			/>
+			<BottomNavigationAction
+				icon={<Resume />}
+				className={classes.root}
+				onClick={() => window.open(resume)}
+			/>
 		</BottomNavigation>
 	);
 };
